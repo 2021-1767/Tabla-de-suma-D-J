b3761c20ac71515b089e952d6cab2dbbafa0de69
Jesus 23 <107210122+2021-1767@users.noreply.github.com>
2022-08-12 10:02:16 -0400

    Add files via upload

---
+++ Tabla D Jesus/AppTabla/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace AppTabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtfila1 = new System.Windows.Forms.TextBox();
+            this.txtnum3 = new System.Windows.Forms.TextBox();
+            this.txtfila2 = new System.Windows.Forms.TextBox();
+            this.txtfila3 = new System.Windows.Forms.TextBox();
+            this.txtnum6 = new System.Windows.Forms.TextBox();
+            this.txtnum5 = new System.Windows.Forms.TextBox();
+            this.txtnum4 = new System.Windows.Forms.TextBox();
+            this.txtnum12 = new System.Windows.Forms.TextBox();
+            this.txtnum11 = new System.Windows.Forms.TextBox();
+            this.txtnum10 = new System.Windows.Forms.TextBox();
+            this.txtnum9 = new System.Windows.Forms.TextBox();
+            this.txtnum8 = new System.Windows.Forms.TextBox();
+            this.txtnum7 = new System.Windows.Forms.TextBox();
+            this.txtnum15 = new System.Windows.Forms.TextBox();
+            this.txtnum14 = new System.Windows.Forms.TextBox();
+            this.txtnum13 = new System.Windows.Forms.TextBox();
+            this.txtfila4 = new System.Windows.Forms.TextBox();
+            this.txtfila5 = new System.Windows.Forms.TextBox();
+            this.txtcolumna1 = new System.Windows.Forms.TextBox();
+            this.txtcolumna2 = new System.Windows.Forms.TextBox();
+            this.txtcolumna3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum1.Location = new System.Drawing.Point(18, 64);
+            this.txtnum1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(121, 49);
+            this.txtnum1.TabIndex = 0;
+            this.txtnum1.TextChanged += new System.EventHandler(this.txtnum1_TextChanged);
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum2.Location = new System.Drawing.Point(139, 64);
+            this.txtnum2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(122, 49);
+            this.txtnum2.TabIndex = 1;
+            this.txtnum2.TextChanged += new System.EventHandler(this.txtnum2_TextChanged);
+            // 
+            // txtfila1
+            // 
+            this.txtfila1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfila1.Location = new System.Drawing.Point(412, 64);
+            this.txtfila1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtfila1.Multiline = true;
+            this.txtfila1.Name = "txtfila1";
+            this.txtfila1.Size = new System.Drawing.Size(132, 39);
+            this.txtfila1.TabIndex = 2;
+            this.txtfila1.TextChanged += new System.EventHandler(this.txtfila1_TextChanged);
+            // 
+            // txtnum3
+            // 
+            this.txtnum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum3.Location = new System.Drawing.Point(261, 64);
+            this.txtnum3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum3.Name = "txtnum3";
+            this.txtnum3.Size = new System.Drawing.Size(122, 49);
+            this.txtnum3.TabIndex = 3;
+            this.txtnum3.TextChanged += new System.EventHandler(this.txtnum3_TextChanged);
+            // 
+            // txtfila2
+            // 
+            this.txtfila2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfila2.Location = new System.Drawing.Point(402, 123);
+            this.txtfila2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtfila2.Name = "txtfila2";
+            this.txtfila2.Size = new System.Drawing.Size(132, 34);
+            this.txtfila2.TabIndex = 16;
+            // 
+            // txtfila3
+            // 
+            this.txtfila3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfila3.Location = new System.Drawing.Point(402, 172);
+            this.txtfila3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtfila3.Name = "txtfila3";
+            this.txtfila3.Size = new System.Drawing.Size(132, 34);
+            this.txtfila3.TabIndex = 17;
+            // 
+            // txtnum6
+            // 
+            this.txtnum6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum6.Location = new System.Drawing.Point(262, 113);
+            this.txtnum6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum6.Name = "txtnum6";
+            this.txtnum6.Size = new System.Drawing.Size(122, 49);
+            this.txtnum6.TabIndex = 20;
+            this.txtnum6.TextChanged += new System.EventHandler(this.txtnum6_TextChanged);
+            // 
+            // txtnum5
+            // 
+            this.txtnum5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum5.Location = new System.Drawing.Point(140, 113);
+            this.txtnum5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum5.Name = "txtnum5";
+            this.txtnum5.Size = new System.Drawing.Size(122, 49);
+            this.txtnum5.TabIndex = 19;
+            this.txtnum5.TextChanged += new System.EventHandler(this.txtnum5_TextChanged);
+            // 
+            // txtnum4
+            // 
+            this.txtnum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum4.Location = new System.Drawing.Point(18, 113);
+            this.txtnum4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum4.Name = "txtnum4";
+            this.txtnum4.Size = new System.Drawing.Size(122, 49);
+            this.txtnum4.TabIndex = 18;
+            this.txtnum4.TextChanged += new System.EventHandler(this.txtnum4_TextChanged);
+            // 
+            // txtnum12
+            // 
+            this.txtnum12.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum12.Location = new System.Drawing.Point(262, 211);
+            this.txtnum12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum12.Name = "txtnum12";
+            this.txtnum12.Size = new System.Drawing.Size(122, 49);
+            this.txtnum12.TabIndex = 26;
+            this.txtnum12.TextChanged += new System.EventHandler(this.txtnum12_TextChanged);
+            // 
+            // txtnum11
+            // 
+            this.txtnum11.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum11.Location = new System.Drawing.Point(141, 211);
+            this.txtnum11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum11.Name = "txtnum11";
+            this.txtnum11.Size = new System.Drawing.Size(121, 49);
+            this.txtnum11.TabIndex = 25;
+            this.txtnum11.TextChanged += new System.EventHandler(this.txtnum11_TextChanged);
+            // 
+            // txtnum10
+            // 
+            this.txtnum10.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum10.Location = new System.Drawing.Point(18, 211);
+            this.txtnum10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum10.Name = "txtnum10";
+            this.txtnum10.Size = new System.Drawing.Size(123, 49);
+            this.txtnum10.TabIndex = 24;
+            this.txtnum10.TextChanged += new System.EventHandler(this.txtnum10_TextChanged);
+            // 
+            // txtnum9
+            // 
+            this.txtnum9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum9.Location = new System.Drawing.Point(262, 162);
+            this.txtnum9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum9.Name = "txtnum9";
+            this.txtnum9.Size = new System.Drawing.Size(122, 49);
+            this.txtnum9.TabIndex = 23;
+            this.txtnum9.TextChanged += new System.EventHandler(this.txtnum9_TextChanged);
+            // 
+            // txtnum8
+            // 
+            this.txtnum8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum8.Location = new System.Drawing.Point(140, 162);
+            this.txtnum8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum8.Name = "txtnum8";
+            this.txtnum8.Size = new System.Drawing.Size(122, 49);
+            this.txtnum8.TabIndex = 22;
+            this.txtnum8.TextChanged += new System.EventHandler(this.txtnum8_TextChanged);
+            // 
+            // txtnum7
+            // 
+            this.txtnum7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum7.Location = new System.Drawing.Point(18, 162);
+            this.txtnum7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum7.Name = "txtnum7";
+            this.txtnum7.Size = new System.Drawing.Size(122, 49);
+            this.txtnum7.TabIndex = 21;
+            this.txtnum7.TextChanged += new System.EventHandler(this.txtnum7_TextChanged);
+            // 
+            // txtnum15
+            // 
+            this.txtnum15.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum15.Location = new System.Drawing.Point(262, 260);
+            this.txtnum15.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum15.Name = "txtnum15";
+            this.txtnum15.Size = new System.Drawing.Size(122, 49);
+            this.txtnum15.TabIndex = 29;
+            this.txtnum15.TextChanged += new System.EventHandler(this.txtnum15_TextChanged);
+            // 
+            // txtnum14
+            // 
+            this.txtnum14.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum14.Location = new System.Drawing.Point(141, 260);
+            this.txtnum14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum14.Name = "txtnum14";
+            this.txtnum14.Size = new System.Drawing.Size(121, 49);
+            this.txtnum14.TabIndex = 28;
+            this.txtnum14.TextChanged += new System.EventHandler(this.txtnum14_TextChanged);
+            // 
+            // txtnum13
+            // 
+            this.txtnum13.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnum13.Location = new System.Drawing.Point(21, 260);
+            this.txtnum13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtnum13.Name = "txtnum13";
+            this.txtnum13.Size = new System.Drawing.Size(120, 49);
+            this.txtnum13.TabIndex = 27;
+            this.txtnum13.TextChanged += new System.EventHandler(this.txtnum13_TextChanged);
+            // 
+            // txtfila4
+            // 
+            this.txtfila4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfila4.Location = new System.Drawing.Point(412, 221);
+            this.txtfila4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtfila4.Name = "txtfila4";
+            this.txtfila4.Size = new System.Drawing.Size(132, 34);
+            this.txtfila4.TabIndex = 30;
+            // 
+            // txtfila5
+            // 
+            this.txtfila5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfila5.Location = new System.Drawing.Point(412, 270);
+            this.txtfila5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtfila5.Multiline = true;
+            this.txtfila5.Name = "txtfila5";
+            this.txtfila5.Size = new System.Drawing.Size(132, 39);
+            this.txtfila5.TabIndex = 31;
+            // 
+            // txtcolumna1
+            // 
+            this.txtcolumna1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcolumna1.Location = new System.Drawing.Point(20, 349);
+            this.txtcolumna1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcolumna1.Name = "txtcolumna1";
+            this.txtcolumna1.Size = new System.Drawing.Size(118, 34);
+            this.txtcolumna1.TabIndex = 32;
+            // 
+            // txtcolumna2
+            // 
+            this.txtcolumna2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcolumna2.Location = new System.Drawing.Point(140, 349);
+            this.txtcolumna2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcolumna2.Name = "txtcolumna2";
+            this.txtcolumna2.Size = new System.Drawing.Size(121, 34);
+            this.txtcolumna2.TabIndex = 33;
+            // 
+            // txtcolumna3
+            // 
+            this.txtcolumna3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcolumna3.Location = new System.Drawing.Point(263, 349);
+            this.txtcolumna3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtcolumna3.Name = "txtcolumna3";
+            this.txtcolumna3.Size = new System.Drawing.Size(122, 34);
+            this.txtcolumna3.TabIndex = 34;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(581, 427);
+            this.Controls.Add(this.txtcolumna3);
+            this.Controls.Add(this.txtcolumna2);
+            this.Controls.Add(this.txtcolumna1);
+            this.Controls.Add(this.txtfila5);
+            this.Controls.Add(this.txtfila4);
+            this.Controls.Add(this.txtnum15);
+            this.Controls.Add(this.txtnum14);
+            this.Controls.Add(this.txtnum13);
+            this.Controls.Add(this.txtnum12);
+            this.Controls.Add(this.txtnum11);
+            this.Controls.Add(this.txtnum10);
+            this.Controls.Add(this.txtnum9);
+            this.Controls.Add(this.txtnum8);
+            this.Controls.Add(this.txtnum7);
+            this.Controls.Add(this.txtnum6);
+            this.Controls.Add(this.txtnum5);
+            this.Controls.Add(this.txtnum4);
+            this.Controls.Add(this.txtfila3);
+            this.Controls.Add(this.txtfila2);
+            this.Controls.Add(this.txtnum3);
+            this.Controls.Add(this.txtfila1);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "TablaDJesus";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtfila1;
+        private System.Windows.Forms.TextBox txtnum3;
+        private System.Windows.Forms.TextBox txtfila2;
+        private System.Windows.Forms.TextBox txtfila3;
+        private System.Windows.Forms.TextBox txtnum6;
+        private System.Windows.Forms.TextBox txtnum5;
+        private System.Windows.Forms.TextBox txtnum4;
+        private System.Windows.Forms.TextBox txtnum12;
+        private System.Windows.Forms.TextBox txtnum11;
+        private System.Windows.Forms.TextBox txtnum10;
+        private System.Windows.Forms.TextBox txtnum9;
+        private System.Windows.Forms.TextBox txtnum8;
+        private System.Windows.Forms.TextBox txtnum7;
+        private System.Windows.Forms.TextBox txtnum15;
+        private System.Windows.Forms.TextBox txtnum14;
+        private System.Windows.Forms.TextBox txtnum13;
+        private System.Windows.Forms.TextBox txtfila4;
+        private System.Windows.Forms.TextBox txtfila5;
+        private System.Windows.Forms.TextBox txtcolumna1;
+        private System.Windows.Forms.TextBox txtcolumna2;
+        private System.Windows.Forms.TextBox txtcolumna3;
+    }
+}
+
